c7a3804055b529d0aeb7d5de8d14cdd11863bac9
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2023-05-14 02:26:55 +0800

    added websocket support and interface for trial

--- Processing_function.py
+++ Processing_function.py
@@ -8,45 +8,62 @@ import re
 from Utils import *
 
 API_KEY = 'e3a053cd179f43b4add5dd0246dd7922'
-API_KEY_1 = 'at_PdWcAdzDk3xzCz89dYLStLa3f53tA'
+API_KEY_1 = 'at_mFL8Qi9LIHjC6saXgawUmjeDyZ5bf'
+API_KEY_1 = 'this is not free'
+API_KEY='not free either'
 
 
 def get_geoinfo(ip):
-    geo_info = requests.get(
-        f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip}&fields=isp,organization,asn,country_code2').json()
-    return geo_info
+    try:
+        geo_info = requests.get(
+            f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip}&fields=isp,organization,asn,country_code2').json()
+        return geo_info
+    except Exception as e:
+        print(e)
+        return []
 
 
 def get_subdomain(url):
-    subdomain_resp = requests.get(
-        f'https://subdomains.whoisxmlapi.com/api/v1?apiKey={API_KEY_1}&domainName={url}').json()
-    subdomain_resp = subdomain_resp.get('result')['records']
-    subdomain = []
-    for response in subdomain_resp:
-        result = response.get('domain')
-        subdomain.append(result)
+    try:
+        subdomain_resp = requests.get(
+            f'https://subdomains.whoisxmlapi.com/api/v1?apiKey={API_KEY_1}&domainName={url}').json()
+        print(subdomain_resp)
+        subdomain_resp = subdomain_resp.get('result')['records']
+        subdomain = []
+        for response in subdomain_resp:
+            result = response.get('domain')
+            subdomain.append(result)
 
-    return subdomain
+        return subdomain
+    except Exception as e:
+        print(e)
+        print('error happended at subdomain')
+        return []
 
 
 def get_asset(url_suffix, url):
-    output = {
-        'anchors': [],
-        'iframes': [],
-        'images': [],
-        'javascripts': [],
-        'stylesheets': [],
-    }
-    html = urlopen(url)
-    content = BeautifulSoup(html, 'html.parser')
-    for keys in output:
-        print(keys)
-        if(config.get(keys)[-1] == 'src'):
-            temp_result = get_src(content, url_suffix, keys)
-            temp_result = checking(temp_result)
-            output[keys].extend(temp_result)
-        elif (config.get(keys)[-1] == 'href'):
-            temp_result = get_href(content, url_suffix, keys)
-            temp_result = checking(temp_result)
-            output[keys].extend(temp_result)
-    return output
+    try:
+        output = {
+            'anchors': [],
+            'iframes': [],
+            'images': [],
+            'javascripts': [],
+            'stylesheets': [],
+        }
+        html = urlopen(url)
+        content = BeautifulSoup(html, 'html.parser')
+        for keys in output:
+            print(keys)
+            if(config.get(keys)[-1] == 'src'):
+                temp_result = get_src(content, url_suffix, keys)
+                temp_result = checking(temp_result)
+                output[keys].extend(temp_result)
+            elif (config.get(keys)[-1] == 'href'):
+                temp_result = get_href(content, url_suffix, keys)
+                temp_result = checking(temp_result)
+                output[keys].extend(temp_result)
+        return output
+    except Exception as e:
+        print(e)
+        print('error at asset retrieval')
+        return output
--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from Processing_function import *
 from Utils import *
+from flask_sock import Sock
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
+sock = Sock(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
+def create_msg(msg):
+    response = {'data': ''}
+    response['data'] = msg
+    return response
 
 
 @app.route('/')
@@ -17,4 +27,32 @@ def url_analyzer():
 
     final = grouped_output(geo_info, subdomain, asset)
 
-    return jsonify(final)
+    return jsonify(**final)
+
+
+@app.route('/ws')
+def index():
+    return render_template('index.html')
+
+
+@sock.route('/echo')
+def echo(sock):
+    while True:
+        data = sock.receive()
+        data = json.loads(data)
+        if data.get("url") is not None:
+            url = data['url']
+            response = create_msg(f'session crated for {url} ')
+            domain_suffix, url_suffix = split_url(url)
+            ip = get_ip(domain_suffix)
+        elif data.get("operation") is not None:
+            if(data['operation'] == 'get_info'):
+                geo_info = get_geoinfo(ip)
+                response = create_msg(f'session crated for {geo_info} ')
+            elif(data['operation'] == 'get_subdomains'):
+                subdomain = get_subdomain(domain_suffix)
+                response = create_msg(f'session crated for {subdomain} ')
+            elif(data['operation'] == 'get_asset_domains'):
+                asset = get_asset(url_suffix, url)
+                response = create_msg(f'session crated for {asset} ')
+        sock.send(response)
